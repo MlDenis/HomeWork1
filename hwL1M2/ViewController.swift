c4ef2552dce07f850f9679d63695d0946f4a51c1
Denis Malyavin <malyavin.dd@outlook.com>
2022-01-26 21:25:15 +0300

    Initial Commit

---
+++ hwL1M2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hwL1M2
+//
+//  Created by Denis Malyavin on 26.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
